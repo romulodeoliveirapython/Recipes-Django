9f29d37675db81b528736c153e6bc2cdbd3deaa0
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-01-26 11:50:48 -0300

    alteração quanto aos arquivos estaticos

--- projeto/settings.py
+++ projeto/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'base_templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
