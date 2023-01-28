cc7cd3f5a131639fa5c7d02a33cfe76859a2d989
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-01-27 23:03:10 -0300

    criação do namespace

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),
-    path('recipe/<int:id>/', views.recipe),
+    path('', views.home, name = 'home'),
+    path('recipe/<int:id>/', views.recipe, name = 'recipe'),
 ]
