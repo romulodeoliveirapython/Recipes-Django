fc0d273e72d1ba81319b90ccaa2ea6ab35309cef
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-01-28 00:12:41 -0300

    configuração da rota para imagens no diretorio /media/

--- projeto/settings.py
+++ projeto/settings.py
@@ -117,10 +117,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
     # BASE_DIR / "static",
     BASE_DIR / "base_static",
 ]
+
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
