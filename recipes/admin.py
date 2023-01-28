6d5cb0c103ecaebe943edd8574cf506db6746d05
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-01-28 00:14:32 -0300

    inclusão dos models no site admin do django de diferentes maneiras

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Recipe
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+admin.site.register(Category, CategoryAdmin)
