c6db4eb3ae7ecfd9c728aadd73e5dd63612e262b
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-01-30 21:53:34 -0300

    alteração da home view

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Recipe
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    recipes = Recipe.objects.all().order_by('-id')
+    return render(request,
+    'recipes/pages/home.html',
+    context = {
+        'recipes': recipes,
+    }
+)
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html')
