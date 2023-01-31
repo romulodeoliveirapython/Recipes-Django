8feba8f99946bc7aadfa3f77e4f71b9457e6ea3e
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-01-31 11:26:13 -0300

    alteração da variavel created_at para que consiga guardar a hora de criação também

--- recipes/models.py
+++ recipes/models.py
@@ -19,7 +19,7 @@ class Recipe(models.Model):
     serving_unit = models.CharField(max_length = 50)
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default = False)
-    created_at = models.DateField(auto_now_add = True)
+    created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateField(auto_now = True)
     is_published = models.BooleanField(default = False)
     cover = models.ImageField(upload_to = 'recipes/covers/%Y/%m/%d/')
